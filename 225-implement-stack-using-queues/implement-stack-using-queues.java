0698296fcb10e468750a1594f33d2e40e45b5a6e
Harilogesh <harilogesh70@gmail.com>
2026-06-06 21:23:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (7.84%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -35,6 +35,49 @@ class MyStack {
  * MyStack obj = new MyStack();
  * obj.push(x);
  * int param_2 = obj.pop();
- * int param_3 = obj.top();
- * boolean param_4 = obj.empty();
- */
+//  * int param_3 = obj.top();
+//  * boolean param_4 = obj.empty();
+//  */
+//  import java.util.*;
+
+// class MyStack {
+//     Queue<Integer> q1 = new LinkedList<>();
+//     Queue<Integer> q2 = new LinkedList<>();
+
+//     public void push(int x) {
+//         q1.offer(x);   // O(1)
+//     }
+
+//     public int pop() {
+//         while (q1.size() > 1) {
+//             q2.offer(q1.poll());
+//         }
+
+//         int top = q1.poll();
+
+//         Queue<Integer> temp = q1;
+//         q1 = q2;
+//         q2 = temp;
+
+//         return top;
+//     }
+
+//     public int top() {
+//         while (q1.size() > 1) {
+//             q2.offer(q1.poll());
+//         }
+
+//         int top = q1.peek();
+//         q2.offer(q1.poll());
+
+//         Queue<Integer> temp = q1;
+//         q1 = q2;
+//         q2 = temp;
+
+//         return top;
+//     }
+
+//     public boolean empty() {
+//         return q1.isEmpty();
+//     }
+// }
